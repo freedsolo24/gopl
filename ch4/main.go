3c5976fbba495e002315b255e34cb926e90ab557
freesouru <freedsolo24@gmail.com>
2025-07-01 12:06:59 +0800

    7/1/2025

--- ch4/main.go
+++ ch4/main.go
@@ -1,9 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	sum1, sum0 := convshaPopcount("abc")
-	fmt.Printf("字符串哈希后的1和0各有: 1有%d个bit, 0有%d个bit\n", sum1, sum0)
-	fmt.Printf("两个字符串哈希值不同的bit数:%d\n", diffbit("abc", "abcd"))
+	// // sum1, sum0 := convshaPopcount("abc")
+	// // fmt.Printf("字符串哈希后的1和0各有: 1有%d个bit, 0有%d个bit\n", sum1, sum0)
+	// // fmt.Printf("两个字符串哈希值不同的bit数:%d\n", diffbit("abc", "abcd"))
+	// flagstdin()
+	flagAlgo()
 }
